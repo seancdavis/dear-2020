850d6086b5ec71760a61118644137915c689f1db
Sean C Davis <scdavis41@gmail.com>
2020-12-18 09:00:48 -0500

    Add some style to the letters view

---
+++ components/icon.js
@@ -0,0 +1,24 @@
+const icons = {
+  arrowRight: (
+    <g stroke="none" stroke-width="1">
+      <polygon points="138.933333 11 108.565333 41.1653333 174.442667 107.042667 0 107.042667 0 149.624 174.442667 149.624 108.565333 215.501333 138.933333 245.666667 256 128.333333"></polygon>
+    </g>
+  )
+}
+
+const Icon = ({ name, ...props }) => {
+  return (
+    <svg
+      viewBox="0 0 256 256"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      {...props}
+    >
+      <title>{name}</title>
+      {icons[name]}
+    </svg>
+  )
+}
+
+export default Icon
--- pages/letters.js
+++ pages/letters.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import gql from "graphql-tag"
 import { useQuery } from "@apollo/client"
 
+import Icon from "../components/icon"
 import Layout from "../components/layout"
 
 const GET_LETTERS_QUERY = gql`
@@ -13,19 +14,43 @@ const GET_LETTERS_QUERY = gql`
   }
 `
 
-const LettersPage = () => {
-  const { loading, error, data } = useQuery(GET_LETTERS_QUERY)
-
-  if (loading) return "Loading..."
-  if (error) return `Error! ${error.message}`
-  // console.log(data)
-
+const Letter = (letter) => {
   const letterSnippet = (letter) => {
     let snippet = letter.body.slice(0, 100)
     if (letter.body.length > 100) {
       snippet += " ..."
     }
     return snippet
+  }
+
+  return (
+    <article className="bg-gray-200 text-gray-800 p-4 rounded-sm shadow-md flex flex-col justify-between">
+      <div className="mb-2" dangerouslySetInnerHTML={{ __html: letterSnippet(letter) }} />
+      <a href={`/letters/${letter.id}`}>
+        <span className="inline-block align-middle mr-2 underline">View Letter</span>
+        <Icon name="arrowRight" className="inline-block w-4" />
+      </a>
+    </article>
+  )
+}
+
+const LettersPage = () => {
+  const { loading, error, data } = useQuery(GET_LETTERS_QUERY)
+
+  let body
+
+  if (loading) {
+    body = <p>"Loading..."</p>
+  } else if (error) {
+    body = <p>Error loading letters. Please refresh the page.</p>
+  } else {
+    body = (
+      <div className="grid grid-cols-4 gap-6 auto-rows-fr">
+        {data.letters.map((letter, idx) => (
+          <Letter {...letter} key={idx} />
+        ))}
+      </div>
+    )
   }
 
   return (
@@ -36,15 +61,7 @@ const LettersPage = () => {
       </Head>
 
       <div>
-        <h1>Letters</h1>
-        <div>
-          {data.letters.map((letter, idx) => (
-            <article key={idx}>
-              <div dangerouslySetInnerHTML={{ __html: letterSnippet(letter) }} />
-              <a href={`/letters/${letter.id}`}>Read More</a>
-            </article>
-          ))}
-        </div>
+        <div className="container mx-auto">{body}</div>
       </div>
     </Layout>
   )
