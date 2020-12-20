58ade1f3cc0999c6d2fad8574110305ac849ea02
Sean C Davis <scdavis41@gmail.com>
2020-12-20 14:46:44 -0500

    [Fixes #1] Add support for signature and email on letters

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
-import { useRef, useState } from "react"
+import { useState } from "react"
 import gql from "graphql-tag"
 import { useMutation } from "@apollo/client"
-import ContentEditable from "react-contenteditable"
 
-import Button from "../components/button"
 import Layout from "../components/layout"
 
 const CREATE_LETTER_MUTATION = gql`
-  mutation CreateLetter($body: String!) {
-    letter: insert_letters_one(object: { body: $body }) {
+  mutation CreateLetter($body: String!, $signature: String!, $email: String) {
+    letter: insert_letters_one(object: { body: $body, signature: $signature, email: $email }) {
       id
     }
   }
 `
 
 const HomePage = () => {
-  const [body, setBody] = useState("")
+  const initFormData = { body: "", signature: "", email: "" }
+  const [formData, setFormData] = useState(initFormData)
   const [status, setStatus] = useState(
     <p>
       Write a letter to 2020 in the box below. Don't be afraid to tell 2020 how you really feel. (
@@ -25,15 +24,12 @@ const HomePage = () => {
       ?)
     </p>
   )
-  const editableRef = useRef(null)
   const [createLetter] = useMutation(CREATE_LETTER_MUTATION)
 
-  const handleLetterSubmit = async () => {
-    if (!body || body.length === 0) {
-      return alert("GIMME A BODY")
-    }
-    const { data: newLetterData } = await createLetter({ variables: { body } })
-    setBody("")
+  const handleLetterSubmit = async (event) => {
+    event.preventDefault()
+    const { data: newLetterData } = await createLetter({ variables: formData })
+    setFormData(initFormData)
     setStatus(
       <p>
         Your letter has been sent.{" "}
@@ -50,21 +46,55 @@ const HomePage = () => {
       <div className="container mx-auto">
         <div className="max-w-md mx-auto mb-8 rounded-sm">{status}</div>
 
-        <div className="max-w-md mx-auto bg-gray-50 text-gray-800 mb-8 rounded-sm">
-          <ContentEditable
-            innerRef={editableRef}
-            html={body}
-            disabled={false}
-            onChange={(event) => setBody(event.target.value)}
-            tagName="div"
-            style={{ minHeight: "200px" }}
-            className="p-4"
+        <form onSubmit={handleLetterSubmit} className="max-w-md mx-auto">
+          <textarea
+            name="body"
+            placeholder="Dear 2020, ..."
+            className="bg-gray-50 text-gray-800 p-4 rounded-sm w-full h-56 mb-4"
+            required
+            onChange={(event) => setFormData({ ...formData, body: event.target.value })}
+            value={formData.body}
           />
-        </div>
-
-        <div className="text-center">
-          <Button onClick={handleLetterSubmit}>Submit Letter</Button>
-        </div>
+          <div className="mb-4">
+            <label htmlFor="#signature" className="block mb-1">
+              Signature
+            </label>
+            <input
+              type="text"
+              id="signature"
+              className="block py-2 px-4 text-gray-800 mb-1 w-full rounded-sm"
+              placeholder="Type your name ..."
+              required
+              onChange={(event) => setFormData({ ...formData, signature: event.target.value })}
+              value={formData.signature}
+            />
+            <p className="text-xs italic">This will be shared alongside your letter.</p>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="#email" className="block mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="block py-2 px-4 text-gray-800 mb-1 w-full rounded-sm"
+              placeholder="Type your email address ..."
+              onChange={(event) => setFormData({ ...formData, email: event.target.value })}
+              value={formData.email}
+            />
+            <p className="text-xs italic">
+              Your email address will only be used to ask your permission for reuse.{" "}
+              <a href="/terms">Learn more</a>.
+            </p>
+          </div>
+          <div className="text-center">
+            <input
+              type="submit"
+              value="Submit Letter"
+              className="bg-gray-50 duration-300 hover:bg-opacity-80 inline-block px-4 py-2 rounded-sm text-gray-800 transition-colors"
+            />
+          </div>
+        </form>
       </div>
     </Layout>
   )
--- pages/letters/[id].js
+++ pages/letters/[id].js
@@ -5,19 +5,22 @@ import apolloClient from "../../lib/apollo-client"
 import Layout from "../../components/layout"
 
 const GET_LETTER_QUERY = gql`
-  query GetLetter($id: uuid!) {
+  query GetLetter($id: Int!) {
     letter: letters_by_pk(id: $id) {
       id
       body
+      signature
     }
   }
 `
 
 const LetterPage = ({ letter }) => {
+  console.log(letter)
   return (
     <Layout>
       <div className="max-w-md mx-auto p-4 bg-gray-200 text-gray-800 rounded-sm">
-        <div dangerouslySetInnerHTML={{ __html: letter.body }} />
+        <div dangerouslySetInnerHTML={{ __html: letter.body }} className="mb-4" />
+        {letter.signature && <p>- {letter.signature}</p>}
       </div>
     </Layout>
   )
