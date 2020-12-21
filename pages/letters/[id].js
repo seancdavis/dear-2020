8a46e1cc0bdea198ff9ddaaf655377d3dce6ac1a
Sean C Davis <scdavis41@gmail.com>
2020-12-21 06:46:57 -0500

    [Fixes #9] Add meta info

--- pages/letters/[id].js
+++ pages/letters/[id].js
@@ -17,7 +17,10 @@ const GET_LETTER_QUERY = gql`
 
 const LetterPage = ({ letter }) => {
   return (
-    <Layout>
+    <Layout
+      title={`Letter from ${letter.signature}`}
+      description={`Read a letter sent from ${letter.signature} to 2020. Then write your own letter!`}
+    >
       <div className="max-w-md mx-auto px-6 sm:px-0">
         <div className="p-4 bg-gray-200 text-gray-800 rounded-sm">
           <div dangerouslySetInnerHTML={{ __html: letter.bodyHtml }} className="mb-4" />
